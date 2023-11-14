6e1b5ec1ae1983e7e26ff3a229678699227c074b
amithasanamithasan <amit35-1773@diu.edu.bd>
2023-11-14 13:46:43 +0600

    confirm massage show

--- js/alert.js
+++ js/alert.js
@@ -3,3 +3,15 @@
 const alertUser=()=>{
     alert('more expensive this method!');
 }
+
+const seriousStudent=()=>{
+   const SeriousStudent= confirm('please!  your ID  use in website!')
+   if(SeriousStudent=== true){
+    alert('yes, you have this ');
+   }else{
+     alert('no requirment this fullfil');
+   }
+
+
+}
+
